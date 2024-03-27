562ab7df8724506904ab16002612ec8053dc696f
Alexander Vorobyov <aovorobyov@mail.ru>
2024-03-27 12:47:29 +0300

    main: Replaced Roman's functions

--- Decimal/s21_bigDecimalToDecimal.c
+++ Decimal/s21_bigDecimalToDecimal.c
@@ -2,7 +2,8 @@
 
 // Функция преобразует тип s21_big_decimal в тип s21_decimal
 
-void s21_bigDecimalToDecimal(s21_big_decimal big_decimal, s21_decimal* decimal) {
+void s21_bigDecimalToDecimal(s21_big_decimal big_decimal,
+                             s21_decimal* decimal) {
   decimal->bits[0] = big_decimal.bits[0];
   decimal->bits[1] = big_decimal.bits[1];
   decimal->bits[2] = big_decimal.bits[2];
--- Decimal/s21_decimal.h
+++ Decimal/s21_decimal.h
@@ -1,6 +1,9 @@
 #ifndef S21_DECIMAL
 #define S21_DECIMAL
 
+#define SCALE 0x00ff0000
+#define MINUS 0x80000000
+
 typedef struct {
   int bits[4];
 } s21_decimal;
@@ -40,5 +43,32 @@ int s21_is_greater(s21_decimal value_1, s21_decimal value_2);
 int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2);
 int s21_is_not_equal(s21_decimal value_1, s21_decimal value_2);
 void to_zero(s21_decimal* num);
+
+void s21_normalise(s21_big_decimal* a, s21_big_decimal* b);
+int s21_bigGetScale(s21_big_decimal num);
+void s21_bigSetScale(s21_big_decimal* num, int scale_value);
+void s21_mul10(s21_big_decimal* decimal);
+void s21_shiftLeft(s21_big_decimal* num, int shift_value);
+int s21_bigGetBit(s21_big_decimal num, int bit_index);
+void s21_bigSetBit(s21_big_decimal* num, int bit_index, unsigned value);
+void s21_printBigDecimal(s21_big_decimal decimal);
+int s21_bigIsZero(s21_big_decimal num);
+void s21_bigToZero(s21_big_decimal* num);
+int s21_bigGetSign(s21_big_decimal num);
+void s21_bigSetSign(s21_big_decimal* num, int sign_value);
+void s21_decimalToBigDecimal(s21_decimal decimal, s21_big_decimal* big_decimal);
+void s21_bigDecimalToDecimal(s21_big_decimal big_decimal, s21_decimal* decimal);
+int s21_isOverflow(s21_big_decimal decimal);
+void s21_bitwiseAddition(s21_big_decimal a, s21_big_decimal b,
+                         s21_big_decimal* result);
+void s21_bitwiseSubtraction(s21_big_decimal a, s21_big_decimal b,
+                            s21_big_decimal* result);
+void s21_bitwiseMultiplication(s21_big_decimal a, s21_big_decimal b,
+                               s21_big_decimal* result);
+void s21_bitwiseDivision(s21_big_decimal a, s21_big_decimal b,
+                         s21_big_decimal* result);
+s21_big_decimal s21_divide(s21_big_decimal a, s21_big_decimal b,
+                           s21_big_decimal* result);
+s21_big_decimal s21_bigDiv10(s21_big_decimal* decimal);
 
 #endif  // S21_DECIMAL
