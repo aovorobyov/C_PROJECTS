2d6468e81cc7c554c5f53e1a1d8e6144a1f4151f
Alexander Vorobyov <aovorobyov@mail.ru>
2024-03-27 12:44:19 +0300

    main: Added posidons functions

--- Decimal/s21_decimal.h
+++ Decimal/s21_decimal.h
@@ -12,15 +12,22 @@ typedef struct {
 #include <stdio.h>
 #include <stdlib.h>
 
-int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
-int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
-int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
-int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
-int AddSubNeg(s21_decimal negative, s21_decimal positive, s21_decimal* result,
-              int is_sub);
-int AddOrSub(s21_decimal value_1, s21_decimal value_2, s21_decimal* result,
-             int is_sub);
-int CorrectOverflow(s21_big_decimal* result_big, int scale, int sign);
+int s21_sub(s21_decimal value_1, s21_decimal value_2,
+            s21_decimal* result);  // corriele
+int s21_mul(s21_decimal value_1, s21_decimal value_2,
+            s21_decimal* result);  // corriele
+int s21_div(s21_decimal value_1, s21_decimal value_2,
+            s21_decimal* result);  // corriele
+int s21_add(s21_decimal value_1, s21_decimal value_2,
+            s21_decimal* result);  // corriele
+int s21_add_sub_neg(s21_decimal negative, s21_decimal positive,
+                    s21_decimal* result,  // corriele
+                    int is_sub);
+int s21_add_or_sub(s21_decimal value_1, s21_decimal value_2,
+                   s21_decimal* result,  // corriele
+                   int is_sub);
+int s21_correct_overflow(s21_big_decimal* result_big, int scale,
+                         int sign);  // corriele
 int get_sign(s21_decimal value);
 void set_bit(s21_decimal* num, int bit_index, unsigned int value);
 int set_scale(s21_decimal* num, unsigned int scale);
